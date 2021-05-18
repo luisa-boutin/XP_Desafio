f20cf1493c20c38dd949514d2ce7f3a1ceb762e6
Luisa Boutin <lboutin23@gmail.com>
2021-05-18 18:13:44 -0300

    Adicionar arquivos de projeto.

---
+++ XP_Projeto/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace XP_Projeto
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nomeLabel = new System.Windows.Forms.Label();
+            this.nomeTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cadastroButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nomeLabel
+            // 
+            this.nomeLabel.AutoSize = true;
+            this.nomeLabel.Location = new System.Drawing.Point(12, 9);
+            this.nomeLabel.Name = "nomeLabel";
+            this.nomeLabel.Size = new System.Drawing.Size(43, 15);
+            this.nomeLabel.TabIndex = 0;
+            this.nomeLabel.Text = "Nome:";
+            // 
+            // nomeTextBox
+            // 
+            this.nomeTextBox.Location = new System.Drawing.Point(61, 6);
+            this.nomeTextBox.Name = "nomeTextBox";
+            this.nomeTextBox.Size = new System.Drawing.Size(100, 23);
+            this.nomeTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(182, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // cadastroButton
+            // 
+            this.cadastroButton.Location = new System.Drawing.Point(182, 28);
+            this.cadastroButton.Name = "cadastroButton";
+            this.cadastroButton.Size = new System.Drawing.Size(75, 23);
+            this.cadastroButton.TabIndex = 3;
+            this.cadastroButton.Text = "Cadastrar";
+            this.cadastroButton.UseVisualStyleBackColor = true;
+            this.cadastroButton.Click += new System.EventHandler(this.cadastroButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(322, 107);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(948, 307);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cadastroButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nomeTextBox);
+            this.Controls.Add(this.nomeLabel);
+            this.HelpButton = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nomeLabel;
+        private System.Windows.Forms.TextBox nomeTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cadastroButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ XP_Projeto/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XP_Projeto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cadastroButton_Click(object sender, EventArgs e)
+        {
+            label1.Text = nomeTextBox.Text;
+            dataGridView1.ColumnCount = 3;
+            dataGridView1.Columns[0].Name = "Nome";
+            dataGridView1.Columns[1].Name = "Telefone";
+            dataGridView1.Columns[2].Name = "CPF";
+            dataGridView1.Rows.Add(nomeTextBox.Text, "12345678", "789456123");
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridView1.Rows.RemoveAt(e.RowIndex);
+        }
+    }
+}
